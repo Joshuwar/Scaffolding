8aca183cd9310e15e4fa98dda36e97489d16cc3b
joshuwar <josh.u.war@gmail.com>
2011-07-07 13:59:49 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+		</div> <!-- close #wrapper -->
+		<script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/jquery-1.6.1.js"></script>
+		<?php wp_footer(); ?>
+		<script type="text/javascript" src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/app.js"></script>
+	</body>
+</html>
